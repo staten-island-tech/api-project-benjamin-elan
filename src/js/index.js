ec64f5fc737982e810a68fdeb8269562628b8de5
BenBerd <71710129+BenBerd@users.noreply.github.com>
2021-05-23 22:56:21 -0400

    boop doop

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "x-a44dd5f8-f213-461b-8bab-2dba83d70530";
+
+const query = async function () {
+    try {
+        const response = await fetch ('https://api.TheDogAPI.com/v1/breeds')
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+        alert("CANDICE");
+    }
+
+};

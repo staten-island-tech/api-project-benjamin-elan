1ffe063a927dac0df8a87bd5cbc622c9ce05b3c3
BenBerd <71710129+BenBerd@users.noreply.github.com>
2021-05-24 00:26:01 -0400

    woop doop

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ const key = "x-a44dd5f8-f213-461b-8bab-2dba83d70530";
 
 const query = async function () {
     try {
-        const response = await fetch ('https://api.TheDogAPI.com/v1/breeds')
+        const response = await fetch ('https://api.thedogapi.com/v1/breeds')
         const data = await response.json();
         console.log(data);
     } catch (error) {
@@ -14,3 +14,4 @@ const query = async function () {
     }
 
 };
+query();

c2b2e8e2798edbf601178a7b3f131e987bc9c744
BenBerd <71710129+BenBerd@users.noreply.github.com>
2021-05-26 23:38:56 -0400

    fantastikk

--- src/index.js
+++ src/index.js
@@ -5,38 +5,40 @@ const key = "x-a44dd5f8-f213-461b-8bab-2dba83d70530";
 // headers["x-a44dd5f8-f213-461b-8bab-2dba83d70530"] = key;
 const query = async function () {
   try {
-    const response = await fetch(
+    let response = await fetch(
       `https://api.thedogapi.com/v1/breeds?api_key=${key}`
     );
-    const data = await response.json();
+    let data = await response.json();
     console.log(data);
-    data.results.forEach((dog) => {
+    data.forEach((dog) => {
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
         `<div class="dog-card">
         <div class="dog-card-front">
           <img
-            src="${dog.image}"
+            src="https://cdn2.thedogapi.com/images/${dog.image.id}.jpg"
             alt=""
             class="poster"
           />
+        </div>
+        
+          <div class="dog-card-back">
+            <h3 class="dog-card-name">${dog.name}</h3>
+            <div class="dog-group">
+              <p class="dog-card-group">${dog.breed_group}</p>
+            </div>
+            <div class="lifespan">
+               <p class="dog-card-lifespan">${dog.life_span}</p>
+            </div>
+            <div class="dog-origin">
+            <p class="dog-card-origin">${dog.origin}</p>
+              <li class="dog-group">Sci-Fi</li>
+              <li class="dog-group">Fantasy</li>
+              <li class="dog-group">Horror</li>
+            </div>
+          </div>
+        </div>
         </div>`
-        //   <div class="dog-card-back">
-        //     <h3 class="dog-card-name">${dog.name}</h3>
-        //     <div class="dog-group">
-        //       <p class="dog-card-group">${dog.breed_group}</p>
-        //     </div>
-        //     <div class="lifespan">
-        //        <p class="dog-card-lifespan">${dog.life_span}</p>
-        //     </div>
-        //     <div class="dog-origin">
-        //     <p class="dog-card-origin">${dog.origin}</p>
-        //       <li class="dog-group">Sci-Fi</li>
-        //       <li class="dog-group">Fantasy</li>
-        //       <li class="dog-group">Horror</li>
-        //     </div>
-        //   </div>
-        // </div>
       );
     });
   } catch (error) {

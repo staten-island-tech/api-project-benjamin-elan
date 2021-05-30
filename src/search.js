0dcbe058019a42962509f05c8168227328a52c5b
ElanRekhtman <71654664+ElanRekhtman@users.noreply.github.com>
2021-05-30 16:03:14 -0400

    not workin atm

--- src/search.js
+++ src/search.js
@@ -1,58 +1,58 @@
-// import { DOMSelectors } from "./js/DOM.js";
-// // import { genres } from "./js/genre.js";
-// const key = "x-a44dd5f8-f213-461b-8bab-2dba83d70530";
-// const listen = function () {
-//   DOMSelectors.searchForm.addEventListener("submit", function (e) {
-//     console.log("submit");
-//     e.preventDefault();
-//     const searchParams = DOMSelectors.searchArea.value;
-//     console.log(searchParams);
-//     const searchQuery = async function () {
-//       console.log("async");
-//       try {
-//         let response = await fetch(
-//           `https://api.thedogapi.com/v1/breeds/search?api_key=${key}?q=${searchParams}`
-//         );
-//         let data = await response.json();
-//         console.log(data);
-//         data.forEach((dog) => {
-//           DOMSelectors.grid.insertAdjacentHTML(
-//             "beforeend",
-//             `<div class="dog-card">
-//               <div class="dog-card-front">
-//                 <img
-//                   src="https://cdn2.thedogapi.com/images/${dog.image.id}.jpg"
-//                   alt=""
-//                   class="dog-card"
-//                 />
-//               </div>
+import { DOMSelectors } from "./js/DOM.js";
+// import { genres } from "./js/genre.js";
+const key = "x-a44dd5f8-f213-461b-8bab-2dba83d70530";
+const listen = function () {
+  DOMSelectors.searchForm.addEventListener("submit", function (e) {
+    console.log("submit");
+    e.preventDefault();
+    const searchParams = DOMSelectors.searchArea.value;
+    console.log(searchParams);
+    const searchQuery = async function () {
+      console.log("async");
+      try {
+        let response = await fetch(
+          `https://api.thedogapi.com/v1/breeds/search?api_key=${key}?q=${searchParams}`
+        );
+        let data = await response.json();
+        console.log(data);
+        data.forEach((dog) => {
+          DOMSelectors.grid.insertAdjacentHTML(
+            "beforeend",
+            `<div class="dog-card">
+              <div class="dog-card-front">
+                <img
+                  src="https://cdn2.thedogapi.com/images/${dog.image.id}.jpg"
+                  alt=""
+                  class="dog-card"
+                />
+              </div>
 
-//                 <div class="dog-card-back">
-//                   <h3 class="dog-card-name">${dog.name}</h3>
-//                   <div class="dog-group">
-//                     <p class="dog-card-group">Group: ${dog.breed_group}</p>
-//                   </div>
-//                   <div class="lifespan">
-//                      <p class="dog-card-lifespan">Life Span: ${dog.life_span}</p>
-//                   </div>
-//                   <div class="dog-origin">
-//                   <p class="dog-card-origin">Origin: ${dog.origin}</p>
-//                     <li class="dog-group">Sci-Fi</li>
-//                     <li class="dog-group">Fantasy</li>
-//                     <li class="dog-group">Horror</li>
-//                   </div>
-//                 </div>
-//               </div>
-//               </div>`
-//           );
-//         });
-//       } catch (error) {
-//         console.log(error);
-//         alert("HOT DOG HOT DOG HOT DIGGITY DOG!!!");
-//       }
-//     };
-//     searchQuery();
-//   });
-// };
+                <div class="dog-card-back">
+                  <h3 class="dog-card-name">${dog.name}</h3>
+                  <div class="dog-group">
+                    <p class="dog-card-group">Group: ${dog.breed_group}</p>
+                  </div>
+                  <div class="lifespan">
+                     <p class="dog-card-lifespan">Life Span: ${dog.life_span}</p>
+                  </div>
+                  <div class="dog-origin">
+                  <p class="dog-card-origin">Origin: ${dog.origin}</p>
+                    <li class="dog-group">Sci-Fi</li>
+                    <li class="dog-group">Fantasy</li>
+                    <li class="dog-group">Horror</li>
+                  </div>
+                </div>
+              </div>
+              </div>`
+          );
+        });
+      } catch (error) {
+        console.log(error);
+        alert("HOT DOG HOT DOG HOT DIGGITY DOG!!!");
+      }
+    };
+    searchQuery();
+  });
+};
 
-// listen();
+listen();
